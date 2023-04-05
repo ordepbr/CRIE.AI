0d2343980c0ebb78e92368ad56e24946454ea269
ORDEP <128438089+ordepbr@users.noreply.github.com>
2023-04-05 12:32:19 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CRIE.AI',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.20.3',
+        'tensorflow==2.7.0',
+        'tensorrt==8.0.1',
+        'opencv-python==4.5.4.58',
+        'scikit-learn==1.0.2',
+        'matplotlib==3.5.1'
+    ],
+    python_requires='>=3.11'
+)
